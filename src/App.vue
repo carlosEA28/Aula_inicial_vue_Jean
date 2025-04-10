e0b0fd8775194d9e25975f179788e5012e5393a5
carlosEA28 <carloseaduarte28@gmail.com>
2025-04-10 10:52:34 -0300

    criando componente

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import ListaTimes from './components/ListaTimes.vue' // importa o componente
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <!-- Usando o componente -->
+  <ListaTimes />
 
   <RouterView />
 </template>
---
+++ src/components/ListaTimes.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Times</h1>
+
+  <ul>
+    <li>Inter</li>
+    <li>Ibis</li>
+    <li>Juventude</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ListaTimes', //nome do componente
+}
+</script>
+
+<style></style>
